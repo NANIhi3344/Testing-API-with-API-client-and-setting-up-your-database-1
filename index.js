b0590ceb68ef1f8e7edce98f0c2ddfc7de59c7ab
NANI <@github.comgit config --global user.email>
2025-01-28 13:07:15 +0530

    good

--- index.js
+++ index.js
@@ -34,7 +34,7 @@
 
 const express = require('express');
 const { resolve } = require('path');
-
+const students =require('.data.json');
 const app = express();
 const port = 3010;
 
@@ -43,9 +43,17 @@ app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+app.post('/students/above-threshold', (req, res) => {
+  
+  const { threshold } = req.body;
+  const studentsAboveThreshold = students.filter(student => student.total > threshold);
+  
+  res.json({
+    count: studentsAboveThreshold.length,
+    students: studentsAboveThreshold
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
